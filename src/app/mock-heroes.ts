e5bd891aae84dfadc4590339ba3b9cf9f1ed6f5e
keerthana <kaviyakeerthana27@gmail.com>
2021-11-12 18:17:30 +0530

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'kaviya' },
+  { id: 12, name: 'keerthana' },
+  { id: 13, name: 'xyz' },
+  { id: 14, name: 'navya' },
+  { id: 15, name: 'shristi' },
+
+];
